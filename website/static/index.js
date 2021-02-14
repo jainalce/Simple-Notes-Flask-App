c61731f9a14916dd9f0a7e44a1aae0ba825dbe87
Jainal Patel <53384033+jainalce@users.noreply.github.com>
2021-02-14 13:49:35 -0500

    Simple Notes Flask App

---
+++ website/static/index.js
@@ -0,0 +1,13 @@
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
+
+function deleteNote()
+{
+	alert('deleteNote')
+}
